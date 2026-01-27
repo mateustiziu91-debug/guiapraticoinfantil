17c8d5637328e84d8ab1284f87e8cc0779a58b78
mateustiziu91-debug <mateusruiz91@gmail.com>
2026-01-27 03:41:17 -0300

    Create pixel.ts

---
+++ src/pixel.ts
@@ -0,0 +1,30 @@
+export function iniciarPixelMeta() {
+  if (typeof window === 'undefined') return;
+
+  !(function (f: any, b: Document, e: string, v: string, n?: any, t?: any, s?: any) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod
+        ? n.callMethod.apply(n, arguments)
+        : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = true;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = true;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(
+    window,
+    document,
+    'script',
+    'https://connect.facebook.net/en_US/fbevents.js'
+  );
+
+  (window as any).fbq('init', '1464512795248622');
+  (window as any).fbq('track', 'PageView');
+}
