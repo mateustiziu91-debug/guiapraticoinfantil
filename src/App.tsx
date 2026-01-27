ee07cf50b63480226036aece65a58abeace28298
mateustiziu91-debug <mateusruiz91@gmail.com>
2026-01-27 03:41:55 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { iniciarPixelMeta } from './pixel';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
